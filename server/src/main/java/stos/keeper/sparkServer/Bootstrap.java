6f61a548c7ba234dc410f4f515cd0c3a92d8252b
Stuart Osborne <stuart.osborne@infinera.com>
2017-08-08 21:16:40 +0200

    Add gson dependency and serialize planned footballmatches as json.

--- server/src/main/java/stos/keeper/sparkServer/Bootstrap.java
+++ server/src/main/java/stos/keeper/sparkServer/Bootstrap.java
@@ -1,12 +1,19 @@
 package stos.keeper.sparkServer;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFiles;
+import stos.keeper.database.DbPlannedMatchesDAO;
+import stos.keeper.database.PostgresDataSourceImpl;
+import stos.keeper.sparkServer.json.PlannedMatchesResponseTransformer;
+
+import static spark.Spark.*;
 
 public class Bootstrap {
 
     public static void main(String[] args) {
+        DbPlannedMatchesDAO plannedMatchesDAO = new DbPlannedMatchesDAO(new PostgresDataSourceImpl());
         staticFiles.externalLocation(System.getProperty("user.dir") + "/server/src/main/resources/public");
-        get("/plannedmatches", (req, response) -> "Hello World!");
+
+        PlannedMatchesResponseTransformer transformer = new PlannedMatchesResponseTransformer();
+        get("/plannedmatches", (req, response) -> plannedMatchesDAO.getAllPlannedMatches(), transformer);
+        after((req, response) -> response.type("application/json"));
     }
 }
---
+++ server/src/main/java/stos/keeper/sparkServer/json/PlannedMatchesResponseTransformer.java
@@ -0,0 +1,34 @@
+package stos.keeper.sparkServer.json;
+
+import com.google.gson.*;
+import spark.ResponseTransformer;
+
+import java.lang.reflect.Type;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class PlannedMatchesResponseTransformer implements ResponseTransformer {
+
+    private Gson gson;
+
+    public PlannedMatchesResponseTransformer() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(ZonedDateTime.class, new ZoneDateTimeSerializationAdapter())
+                .create();
+    }
+
+    @Override
+    public String render(Object model) throws Exception {
+        return gson.toJson(model);
+    }
+
+    private class ZoneDateTimeSerializationAdapter implements JsonSerializer<ZonedDateTime> {
+        @Override
+        public JsonElement serialize(ZonedDateTime src, Type typeOfSrc, JsonSerializationContext context) {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMM d, yyyy H:mm")
+                    .withZone(ZoneId.systemDefault());;
+            return new JsonPrimitive(src.format(dateTimeFormatter));
+        }
+    }
+}
