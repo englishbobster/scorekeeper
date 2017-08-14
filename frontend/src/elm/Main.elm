1efab99b0d447f2f131ff0e5f042bca30756c8aa
Stuart Osborne <stuart.osborne@infinera.com>
2017-08-14 15:51:15 +0200

    Started fleshing out frontend.

--- frontend/src/elm/Main.elm
+++ frontend/src/elm/Main.elm
@@ -1,7 +1,85 @@
-module Hello exposing (..)
+module ScoreKeeper exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text)
+
+
+--Model
+
+
+type Group
+    = A
+    | B
+    | C
+    | D
+    | E
+    | F
+    | G
+
+
+type MatchType
+    = GroupGame Group
+    | R16
+    | QF
+    | SF
+    | ThirdPlace
+    | Final
+
+
+type alias PlannedMatch =
+    { homeTeam : String
+    , homeScore : Int
+    , awayTeam : String
+    , awayScore : Int
+    , matchTime : String
+    , arena : String
+    , matchType : MatchType
+    }
+
+
+type alias PlannedMatches =
+    List PlannedMatch
+
+
+type alias Model =
+    PlannedMatches
+
+
+initialModel : Model
+initialModel =
+    [ { homeTeam = "Brazil"
+      , homeScore = 1
+      , awayTeam = "Croatia"
+      , awayScore = 0
+      , matchTime = "fix a timestamp"
+      , arena = "amazoa"
+      , matchType = GroupGame A
+      }
+    , { homeTeam = "Sweden"
+      , homeScore = 3
+      , awayTeam = "England"
+      , awayScore = 3
+      , matchTime = "another timestamp"
+      , arena = "sao paulo"
+      , matchType = Final
+      }
+    ]
+
+
+
+--View
+
+
+view : Model -> Html msg
+view model =
+    model
+        |> List.map (\match -> match.homeTeam)
+        |> String.join ","
+        |> text
+
+
+
+--Main
 
 
 main =
-    text "Hello World! from ELM"
+    view initialModel
