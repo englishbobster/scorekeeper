e183b28bd08f014832163056a2e1586b06089ecb
Stuart Osborne <stuart.osborne@infinera.com>
2017-09-01 22:35:46 +0200

    Some refactoring of api routes.

--- server/src/main/java/ScoreKeeper.java
+++ server/src/main/java/ScoreKeeper.java
@@ -2,10 +2,12 @@ import org.eclipse.jetty.server.Response;
 import stos.keeper.database.PlannedMatchesDAO;
 import stos.keeper.database.PlayerDAO;
 import stos.keeper.database.PostgresDataSourceImpl;
+import stos.keeper.model.planned_matches.FootballMatch;
 import stos.keeper.model.planned_matches.Score;
 import stos.keeper.model.player.Player;
 import stos.keeper.sparkServer.json.JsonTransformer;
 
+import java.util.List;
 import java.util.Optional;
 
 import static spark.Spark.after;
@@ -27,12 +29,23 @@ public class ScoreKeeper {
 
         after((request, response) -> response.type("application/json"));
 
-        get("/plannedmatches", (request, response) -> plannedMatchesDAO.getAllPlannedMatches(), transformer);
+        get("/plannedmatches", (request, response) -> {
+            List<FootballMatch> allPlannedMatches = plannedMatchesDAO.getAllPlannedMatches();
+            response.status(Response.SC_OK);
+            return allPlannedMatches;
+        }, transformer);
 
         put("/plannedmatches/:id", (request, response) -> {
             int id = Integer.parseInt(request.params(":id"));
             Score score = transformer.scoreFromJson(request.body());
-            return plannedMatchesDAO.setFinalScoreForMatchById(id, score);
+            int result = plannedMatchesDAO.setFinalScoreForMatchById(id, score);
+            if(result == 0){
+                response.status(Response.SC_NOT_FOUND);
+                return "Match not found.";
+            }else{
+                response.status(Response.SC_OK);
+                return "Match updated.";
+            }
         });
 
         get("/player/:name", (request, response) -> {
