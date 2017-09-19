30f7c57c35a0e4b49206b8dadce9b76e19ffdadb
Stuart Osborne <stuart.osborne@infinera.com>
2017-09-19 19:30:08 +0200

    More tests for login route added.

--- server/src/test/java/stos/keeper/sparkServer/api/routes/LoginPlayerRouteTest.java
+++ server/src/test/java/stos/keeper/sparkServer/api/routes/LoginPlayerRouteTest.java
@@ -54,4 +54,47 @@ public class LoginPlayerRouteTest {
         assertThat(responseData.getResponseMessage(), startsWith(expectedResponse));
     }
 
+    @Test
+    public void unauthorized_is_returned_with_wrong_password() throws Exception {
+        ZonedDateTime time = ZonedDateTime.now();
+        String salt = PasswordService.generateSalt();
+        String encryptedPassword = PasswordService.encryptPassword("dallas", salt);
+        Player player = Player.builder().id(7).username("bobby")
+                .password(encryptedPassword).passwordSalt(salt)
+                .email("another@email.address").hasPaid(true)
+                .created(time).build();
+
+        String requestBody = "{\"username\":\"bobby\"," + "\"password\":\"wrongpassword\"" + "}";
+
+        when(dao.getPlayerByName("bobby")).thenReturn(Optional.of(player));
+        LoginPlayerRoute route = new LoginPlayerRoute(new JsonTransformer(), dao, tokenGenerator);
+        ResponseData responseData = route.process(requestBody, Collections.emptyMap());
+
+        assertThat(responseData.getResponseStatus(), is(Response.SC_UNAUTHORIZED));
+        assertThat(responseData.getResponseMessage(), startsWith("Player not authorized."));
+    }
+
+    @Test
+    public void not_found_is_returned_without_an_existing_player() throws Exception {
+        String requestBody = "{\"username\":\"bobby\"," + "\"password\":\"wrongpassword\"" + "}";
+
+        when(dao.getPlayerByName("bobby")).thenReturn(Optional.empty());
+        LoginPlayerRoute route = new LoginPlayerRoute(new JsonTransformer(), dao, tokenGenerator);
+        ResponseData responseData = route.process(requestBody, Collections.emptyMap());
+
+        assertThat(responseData.getResponseStatus(), is(Response.SC_NOT_FOUND));
+        assertThat(responseData.getResponseMessage(), startsWith("Player not found."));
+
+    }
+
+    @Test
+    public void invalid_is_returned_from_gobbledygook_request() throws Exception {
+        String requestBody = "";
+
+        LoginPlayerRoute route = new LoginPlayerRoute(new JsonTransformer(), dao, tokenGenerator);
+        ResponseData responseData = route.process(requestBody, Collections.emptyMap());
+
+        assertThat(responseData.getResponseStatus(), is(Response.SC_BAD_REQUEST));
+        assertThat(responseData.getResponseMessage(), startsWith("Invalid Request."));
+    }
 }
